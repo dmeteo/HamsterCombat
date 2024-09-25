05120e77bd51023a8d09606a5ef4a5aa9f5baa93
dmeteo <pesh.dmetro@gmail.com>
2024-09-26 00:18:30 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace CSharpClicker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddHealthChecks();
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+            app.MapHealthChecks("health-check");
+
+            app.Run();
+        }
+    }
+}

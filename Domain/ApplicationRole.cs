3c76da2906150b1db5c023e65db577f3af55122f
dmeteo <pesh.dmetro@gmail.com>
2024-10-13 02:42:06 +0500

    db add

---
+++ Domain/ApplicationRole.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CSharpClicker.Domain
+{
+    public class ApplicationRole : IdentityRole<Guid>;
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using CSharpClicker.Domain;
+using CSharpClicker.Infrastructure.DataAccess;
+using CSharpClicker.Initializers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace CSharpClicker
 {
     public class Program
@@ -6,13 +12,25 @@ namespace CSharpClicker
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddHealthChecks();
+            ConfigureServices(builder.Services);
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
             app.MapHealthChecks("health-check");
 
             app.Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+
+            //services.AddIdentity<ApplicationUser, ApplicationRole>()
+            //    .AddEntityFrameworkStores<AppDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            DbContextInitializer.InitializeDbContext(services);
         }
     }
 }
